3ad0084a4f5820b6102fa8ed99f96a60b43054a6
Kazutaka Matsuda <kztk@tohoku.ac.jp>
2024-12-09 20:23:53 +0900

    Cache lenEnv (fixed #5)

--- src/Unembedding/Env.hs
+++ src/Unembedding/Env.hs
@@ -3,59 +3,103 @@
 Generic environment used by Embedding by Unembedding, and functions over it.
 
 -}
+{-# LANGUAGE CPP                  #-}
 {-# LANGUAGE DataKinds            #-}
 {-# LANGUAGE DerivingStrategies   #-}
 {-# LANGUAGE GADTs                #-}
+{-# LANGUAGE PatternSynonyms      #-}
 {-# LANGUAGE PolyKinds            #-}
 {-# LANGUAGE RankNTypes           #-}
 {-# LANGUAGE StandaloneDeriving   #-}
 {-# LANGUAGE TypeFamilies         #-}
 {-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ViewPatterns         #-}
 module Unembedding.Env (
-  Env(..), Ix(..), lookEnv, lenEnv, mapEnv, mapEnvWithIx, fromIndexer,
+  Env, pattern ENil, pattern ECons, Ix(..), lookEnv, lenEnv, mapEnv, mapEnvWithIx, fromIndexer,
   Append, appendEnv,
 
   Func, fromFunc,
   ) where
 import           Data.Kind (Type)
 
--- | A generic environment
+data Env_ (f :: k -> Type) (as :: [k]) where
+  ENil_  :: Env_ f '[]
+  ECons_ :: !(f a) -> !(Env_ f as) -> Env_ f (a ': as)
+
+-- | A generic environment. One can think this datatype is defined as:
+--
+--  > data Env f as where
+--  >   ENil  :: Env f '[]
+--  >   ECons :: !(f a) -> !(Env f as) -> Env f (a : as)
+--
+--  with constructors 'ENil' and 'ECons'.
+--
 --   What this is an env of can be customised by the selection of f
 --     f := Proxy => Type env (no values stored in env)
 --     f := Maybe => value env, where we might not have all assignments
-data Env (f :: k -> Type) (as :: [k]) where
-  ENil  :: Env f '[]
-  ECons :: f a -> Env f as -> Env f (a ': as)
+data Env (f :: k -> Type) (as :: [k]) = MkEnv {-# UNPACK #-} !Int !(Env_ f as) -- caches lenEnv
+
+getEnvBody :: Env f as -> Env_ f as
+getEnvBody (MkEnv _ es) = es
+
+pattern ENil :: () => (as ~ '[]) => Env f as
+pattern ENil <- MkEnv _ ENil_
+  where
+    ENil = MkEnv 0 ENil_
+
+pattern ECons :: () => (as ~ a1 : as1) => f a1 -> Env f as1 -> Env f as
+pattern ECons e es <- MkEnv n (ECons_ e (MkEnv (n-1) -> es))
+  where
+    ECons e es = MkEnv (lenEnv es + 1) (ECons_ e (getEnvBody es))
+
+#if __GLASGOW_HASKELL__ >= 902
+{-# INLINABLE ENil #-}
+{-# INLINABLE ECons #-}
+#endif
+
+{-# COMPLETE ENil, ECons #-}
+
+-- >>> lenEnv $ ECons [1] $ ECons [2] $ ENil
+-- 2
+-- >>> lenEnv $ ENil
+-- 0
+
+-- >>> let ECons _ xs = ECons [1] $ ECons [2] $ ENil
+-- >>> lenEnv xs
+-- 1
 
 -- | (typed) de Bruijn indices witness that a is in env
 data Ix (as :: [k]) (a :: k) where
   IxZ :: Ix (a ': as) a             -- At element in question.
-  IxS :: Ix as a -> Ix (b ': as) a  -- Element lies further into the env.
+  IxS :: !(Ix as a) -> Ix (b ': as) a  -- Element lies further into the env.
 
 deriving stock instance Show (Ix as a)
 
 
 -- | Looking up something in an env using Ix
 lookEnv :: Env f as -> Ix as a -> f a
-lookEnv env0 IxZ = case env0 of
-  ECons v _ -> v
-lookEnv env0 (IxS n) = case env0 of
-  ECons _ env -> lookEnv env n
+lookEnv (MkEnv _ xs) = lookEnv_ xs
+
+lookEnv_ :: Env_ f as -> Ix as a -> f a
+lookEnv_ env0 IxZ = case env0 of
+  ECons_ v _ -> v
+lookEnv_ env0 (IxS n) = case env0 of
+  ECons_ _ env -> lookEnv_ env n
+
 
 -- | Finds the length of an env
 lenEnv :: Env f as -> Int
-lenEnv ENil         = 0
-lenEnv (ECons _ as) = 1 + lenEnv as
-
--- examplef :: (forall x . Proxy x -> Maybe x)
--- examplef _ = Nothing
+lenEnv (MkEnv len _) = len
 
 -- | Changes what sort of env we have
 -- e.g. we could use this to go from a ProxyEnv to a MaybeEnv with (const Nothing)
 mapEnv :: (forall x. f x -> g x) -> Env f as -> Env g as
-mapEnv _ ENil         = ENil
-mapEnv f (ECons x xs) = ECons (f x) (mapEnv f xs)
+mapEnv f0 (MkEnv n es) = MkEnv n (mapEnv_ f0 es)
+
+mapEnv_ :: (forall x. f x -> g x) -> Env_ f as -> Env_ g as
+mapEnv_ _ ENil_         = ENil_
+mapEnv_ f (ECons_ x xs) = ECons_ (f x) (mapEnv_ f xs)
 
 -- | A variant of 'mapEnv' with index.
 mapEnvWithIx :: (forall x. Ix as x -> f x -> g x) -> Env f as -> Env g as
@@ -70,9 +114,13 @@ type family Append as bs where
   Append '[] bs = bs
   Append (a ': as) bs = a ': Append as bs
 
+-- | Append two 'Env's.
 appendEnv :: Env f as -> Env f bs -> Env f (Append as bs)
-appendEnv ENil ys         = ys
-appendEnv (ECons x xs) ys = ECons x (appendEnv xs ys)
+appendEnv (MkEnv lxs xs) (MkEnv lys ys) = MkEnv (lxs + lys) (appendEnv_ xs ys)
+
+appendEnv_ :: Env_ f as -> Env_ f bs -> Env_ f (Append as bs)
+appendEnv_ ENil_ ys         = ys
+appendEnv_ (ECons_ x xs) ys = ECons_ x (appendEnv_ xs ys)
 
 -- | @Func f [a1,...an] r = f a1 -> ... -> f an -> r@
 type family Func sem as r where
