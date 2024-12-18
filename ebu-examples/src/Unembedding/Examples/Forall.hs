685aabf75d3213c099ad8bcb2c137fb8ed411114
Kazutaka Matsuda <kztk@tohoku.ac.jp>
2024-12-18 20:09:45 +0900

    Removed the dependency on defun-core

--- ebu-examples/src/Unembedding/Examples/Forall.hs
+++ ebu-examples/src/Unembedding/Examples/Forall.hs
@@ -40,9 +40,48 @@ import           Unembedding     (LiftVariables (..), Variables, Weakenable,
                                   ol0, ol1)
 import qualified Unembedding.Env as UE
 
-import           DeFun.Core      (App, type (@@), type (~>))
 import           Unembedding.Env (pattern (:.), pattern ENil)
 
+
+-- The code fragment is imported from https://hackage.haskell.org/package/defun-core, with a minor modification.
+-- Copyright (c) 2023, Oleg Grenrus
+-- Licensed under the BSD 3-Clause License. See below for full license text.
+type (~>) (a :: k1) (b :: k2) = Proxy a -> Proxy b -> Type -- minor tweak: Use Proxy instead of FunKind here.
+infixr 0 ~>
+type family App (f :: k1 ~> k2) (a :: k1) :: k2
+type (@@) a b = App a b
+{-
+Copyright (c) 2023, Oleg Grenrus
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of Oleg Grenrus nor the names of other
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+-}
 
 data T = O | T :~> T | Forall (T ~> T)
 
